b97dade36af860bb58cfd7517a9797fe0c233b4a
manglanigaurav01-jpg <manglanigaurav01@gmail.com>
2025-12-29 17:36:28 +0530

    114

--- android/app/src/main/java/com/prakashbhai/billbuddy/MainActivity.java
+++ android/app/src/main/java/com/prakashbhai/billbuddy/MainActivity.java
@@ -1,6 +1,11 @@
 package com.prakashbhai.billbuddy;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
-
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
